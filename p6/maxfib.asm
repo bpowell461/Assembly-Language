960c41403bcf8fbdd5541df590444d36234fe121
Brad Powell <bpowell461@gmail.com>
2020-10-19 23:03:36 -0400

    Project 6

--- p6/maxfib.asm
+++ p6/maxfib.asm
@@ -8,7 +8,7 @@ SECTION .data
 
 SECTION .bss
 
-MaxFib: RESD 1
+MaxFib: RESD 1 ;If I am correct, this should hold up to Fib(47)
 
 SECTION .text
 
@@ -17,17 +17,21 @@ global _start
 _start:
     
     mov eax, 1
-    mov ebx, 0
-    add eax, ebx
-    
-    
+    mov edx, 0
+    add eax, edx
+    mov edx, eax
+
     computeMax:
-        mov eax, ebx
-        add eax, ebx
-        jnc computeMax
-        
-    mov DWORD [MaxFib], ebx    
-	lastBreak:
+	push eax
+        add eax, edx
+	jc lastBreak
+	pop edx
+        jnc computeMax ;This should compute Fibonacci(46) = 1836311903
+
+
+    lastBreak:
+	pop edx ;Fib(47) is in the Stack so we are popping it
+	mov DWORD [MaxFib+0], edx
 
 
 	mov eax, 1
